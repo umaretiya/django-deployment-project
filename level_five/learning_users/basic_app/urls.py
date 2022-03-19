c257a6eca7cc135bf5c4cb7250a58191a20a49b9
Keshav Umaretiya <umaretiya@gmail.com>
2022-03-19 20:30:03 +0530

    'first'

---
+++ level_five/learning_users/basic_app/urls.py
@@ -0,0 +1,12 @@
+from unicodedata import name
+from django.conf.urls import url
+from basic_app import views
+
+
+# Template urls!
+app_name = 'basic_app'
+
+urlpatterns = [
+    url(r'^register/$',views.register,name='register'),
+    url(r'^user_login/$', views.user_login,name='user_login')
+]
